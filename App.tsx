afe10aa61c228b69c020062f0691d737d22d2df0
luanmxz <luanmarcenemxz22@gmail.com>
2023-01-14 23:10:59 -0300

    Added gestures and animations on the tag's list

--- App.tsx
+++ App.tsx
@@ -2,11 +2,19 @@ import { StatusBar } from 'react-native';
 
 import { Home } from './src/screens/Home';
 
+import 'react-native-gesture-handler';
+import { HoldMenuProvider } from 'react-native-hold-menu';
+import { Feather } from '@expo/vector-icons';
+
 export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <Home />
-    </>
-  );
+	return (
+		<HoldMenuProvider theme='dark' iconComponent={Feather}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+			<Home />
+		</HoldMenuProvider>
+	);
 }
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,16 +1,33 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, View } from 'react-native';
+import { HoldItem } from 'react-native-hold-menu';
+import Animated, { FadeIn, FadeOut, Layout } from 'react-native-reanimated';
 
 import { styles } from './styles';
 
 type Props = {
-  title: string;
-  onRemove: () => void;
+	title: string;
+	onRemove: () => void;
 };
 
 export function Tag({ title, onRemove }: Props) {
-  return (
-    <TouchableOpacity  style={styles.container} onPress={onRemove}>
-      <Text style={styles.title}>#{title}</Text>
-    </TouchableOpacity>
-  );
+	return (
+		<Animated.View
+			style={styles.container}
+			layout={Layout}
+			entering={FadeIn}
+			exiting={FadeOut}>
+			<HoldItem
+				items={[
+					{ text: title, isTitle: true },
+					{
+						text: 'Apagar',
+						icon: 'trash',
+						isDestructive: true,
+						onPress: () => onRemove(),
+					},
+				]}>
+				<Text style={styles.title}>#{title}</Text>
+			</HoldItem>
+		</Animated.View>
+	);
 }
